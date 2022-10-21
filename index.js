259e8e6baa7f1e10c53e71ea6ddd7811f33091f3
Jacob Whitlow <jwhitlow45@gmail.com>
2022-10-21 14:06:51 -0700

    feat:add functionality to options buttons

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function onPageLoad() {
   if (localStorage.getItem("board-color") == null) {
-    localStorage.setItem("board-size", "6x7");
-    localStorage.setItem("board-color", "blue");
-    localStorage.setItem("p0-color", "red");
-    localStorage.setItem("p1-color", "yellow");
+    localStorage.setItem("board-size", 0);
+    localStorage.setItem("board-color", 4);
+    localStorage.setItem("p0-color", 0);
+    localStorage.setItem("p1-color", 3);
   }
 }
--- pages/js/options.js
+++ pages/js/options.js
@@ -3,15 +3,49 @@ const board_color = document.getElementById("board-color");
 const p0_color = document.getElementById("p0-color");
 const p1_color = document.getElementById("p1-color");
 
+const colors = [
+  "red",
+  "fuchsia",
+  "lime",
+  "yellow",
+  "blue",
+  "aqua"
+];
+
+const boardSizes = [
+  "6x7",
+  "8x9"
+];
+
 function onPageLoad() {
-  let board_size_setting = localStorage.getItem("board-size");
-  let board_color_setting = localStorage.getItem("board-color");
-  let p0_color_setting = localStorage.getItem("p0-color");
-  let p1_color_setting = localStorage.getItem("p1-color");
+  let board_size_setting = boardSizes[localStorage.getItem("board-size")];
+  let board_color_setting = colors[localStorage.getItem("board-color")];
+  let p0_color_setting = colors[localStorage.getItem("p0-color")];
+  let p1_color_setting = colors[localStorage.getItem("p1-color")];
 
   board_size.innerText = board_size_setting;
   board_color.style.backgroundColor = board_color_setting;
   p0_color.style.backgroundColor = p0_color_setting;
   p1_color.style.backgroundColor = p1_color_setting;
+}
 
+function nextColor(elemId) {
+  var colorIndex = localStorage.getItem(elemId);
+  colorIndex = (parseInt(colorIndex) + 1) % colors.length;
+  localStorage.setItem(elemId, colorIndex);
+
+  if (localStorage.getItem("p0-color") == localStorage.getItem("p1-color")) {
+    colorIndex = (parseInt(colorIndex) + 1) % colors.length;
+    localStorage.setItem(elemId, colorIndex);
+  }
+
+  let htmlelem = document.getElementById(elemId);
+  htmlelem.style.backgroundColor = colors[colorIndex];
+}
+
+function nextBoardSize() {
+  var boardIndex = localStorage.getItem("board-size");
+  boardIndex = (parseInt(boardIndex) + 1) % boardSizes.length;
+  localStorage.setItem("board-size", boardIndex);
+  board_size.innerText = boardSizes[boardIndex];
 }
